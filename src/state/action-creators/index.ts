f775e09a82c1f02e56f5c4d0986c3295129ec1d8
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-04-16 22:49:24 -0600

    Added type dispatch to the dispatch in the searchRepositories action creator

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import { ActionType } from '../acion-types';
-import { Action } from '../action'
+import { Dispatch } from 'redux';
+import { ActionType } from '../action-types';
+import { Action } from '../actions'
+
 
 export const searchRepositories = (term: string) => {
-  return async (dispatch: any) => {
+  return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES
     });
@@ -11,11 +13,11 @@ export const searchRepositories = (term: string) => {
     try {
       const { data } = await axios.get('https://registry.npmjs.org/-/v1/search',
       {
-      params: {
-        text: term
-      },
-    }
-  );
+        params: {
+          text: term
+        },
+      }
+      );
 
       const names = data.objects.map((result: any) => {
         return result.package.name
