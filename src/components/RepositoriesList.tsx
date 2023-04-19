169d8afa94209918b5b7ea2f7c7e1b9c20ad8b3d
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-04-18 21:51:13 -0600

    Created a hook to dispatch an action creator

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,12 +1,17 @@
 
 import { useState, FormEvent } from 'react';
+import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions()
 
 
   const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    // dispatch(actionCreators.searchRepositories(term) as any);
+    searchRepositories(term)
   }
 
   return <div>
