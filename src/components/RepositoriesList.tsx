135bd98211d288dc46974b18a77e6e8c3b70262b
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-04-20 01:15:03 -0600

    Created useTypeSelector for TypeScript

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,25 +1,33 @@
 
-import { useState, FormEvent } from 'react';
+import { useState } from 'react';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
-  const { searchRepositories } = useActions()
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector((state) => state.repositories);
 
-
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    // dispatch(actionCreators.searchRepositories(term) as any);
-    searchRepositories(term)
-  }
+       // dispatch(actionCreators.searchRepositories(term) as any);
+    searchRepositories(term);
+  };
 
-  return <div>
-    <form onSubmit={onSubmit}>
-      <input value={term} onChange={e => setTerm(e.target.value)}/>
-      <button> Search </button>
-    </form>
-  </div>
-}
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+      {error && <h3> {error} </h3>}
+      {loading && <h3> Loading...</h3>}
+      {/* {!error && !loading && 
+      data.map((name) => <div> <div/>)} */}
+      {!error && !loading && data.map((name) => <div key={name}> {name}</div>)}
+    </div>
+  );
+};
 
 export default RepositoriesList;
